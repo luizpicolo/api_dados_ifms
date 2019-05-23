8b4c60d55fe066601943b1641eab14635db88c08
Luiz Picolo <luizpicolo@gmail.com>
2019-05-23 19:31:09 -0400

    Atualizada projeto com novos parametros para a api de dados

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,6 +4,6 @@ class CoursesController < ApplicationController
   def index
     user = User.find(params[:token])
     courses = SigaeduApi::AcademicRegistry::Course.new(user.login, user.password)
-    render json: courses.get_data
+    render json: courses.get_data(params[:campus])
   end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,6 +4,6 @@ class StudentsController < ApplicationController
   def index
     user = User.find(params[:token])
     siga = SigaeduApi::AcademicRegistry::Student.new(user.login, user.password)
-    render json: siga.get_data
+    render json: siga.get_data(params[:campus])
   end
 end
