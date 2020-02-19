349e84f722b13ac74171c2c29e331e4be3343a86
Luiz Picolo <luizpicolo@gmail.com>
2020-02-18 21:09:08 -0400

    Adicionado retorno para dados especificos do estudante

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,7 +3,12 @@ require 'sigaedu_api'
 class StudentsController < ApplicationController
   def index
     user = User.find(params[:token])
-    siga = SigaeduApi::AcademicRegistry::Student.new(user.login, user.password)
-    render json: siga.get_data(params[:campus])
+    if !params[:id]
+      students = SigaeduApi::AcademicRegistry::Student.new(user.login, user.password)
+      render json: students.get_data(params[:campus])
+    else 
+      student = SigaeduApi::AcademicRegistry::StudentData.new(user.login, user.password)
+      render json: student.get_data(params[:id])
+    end
   end
 end
